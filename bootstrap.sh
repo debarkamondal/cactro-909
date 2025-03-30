bbbeb0ea69395eaf75bf1871b1db9ed2bd155cb7
debarka <debarkamondal@gmail.com>
2025-03-30 14:28:31 +0530

    fix script was not executible

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 yum update -y
 yum install unzip awscli cronie git -y
 touch /var/log/app.log
+git clone https://github.com/debarkamondal/cactro-909 ~/cactro-909
+chmod -x ~/cactro-909
 echo -e "*/2 * * * * ~/cactro-909/upload.sh" | crontab -u root -
 systemctl enable crond
-git clone https://github.com/debarkamondal/cactro-909 ~/cactro-909
